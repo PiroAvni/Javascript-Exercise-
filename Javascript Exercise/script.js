19cf73c889c822500fa1b0cbe2c8592a85dfada4
Avni Piro <Avnipiro.dev@gmail.com>
2022-01-05 18:19:53 +0000

    jan update

--- Javascript Exercise/script.js
+++ Javascript Exercise/script.js
@@ -2,6 +2,7 @@
 
 //Write a function that takes two numbers and returns the maximum of the two.
 
+//my way
 const inputNumber1 = Number(prompt("Pick a Number?"));
 const inputNumber2 = Number(prompt("Pick another Number?"));
 console.log(inputNumber1, inputNumber2); 
@@ -12,7 +13,7 @@ function maxNumber (inputNumber1, inputNumber2) {
 
 console.log(maxNumber(inputNumber1, inputNumber2));
 
-////////////////////////////////
+//////////////////////////////// another way
 let number = max(inputNumber1, inputNumber2);
 console.log(number);
 function max (a, b) {
@@ -21,7 +22,7 @@ function max (a, b) {
   return b;
 }
 
-////////////////////////////////
+//////////////////////////////// another way 
 let numbers = max(inputNumber1, inputNumber2);10
 console.log(numbers);
 
