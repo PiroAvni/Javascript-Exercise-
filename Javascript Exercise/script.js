ff62f5e237e42f6b1810f6b95c5a0555175e3de5
Avni Piro <Avnipiro.dev@gmail.com>
2022-03-06 16:30:41 +0000

    update

--- Javascript Exercise/script.js
+++ Javascript Exercise/script.js
@@ -144,7 +144,7 @@ let address = {
 
 // FACTORY function
 
-let address = newAddress("a", "b", "c");
+/* let address = newAddress("a", "b", "c");
 console.log(address);
 
 function newAddress(street, city, zipcode) {
@@ -163,3 +163,29 @@ function Address(street, city, zipcode) {
   this.city = city;
   this.zipcode = zipcode;
 }
+ */
+// Exercise 3 - Object Equality
+
+/* let address1 = new Address("a", "b", "c");
+let address2 = new Address("a", "b", "c");
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+
+function Address(street, city, zipcode) {
+  this.street = street;
+  this.city = city;
+  this.zipcode = zipcode;
+}
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipcode === address2.zipcode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2;
+} */
